7c34a25dc6e560e6454ba372a56d616e7667fc14
MarekHuddleston <131384061+MarekHuddleston@users.noreply.github.com>
2023-05-19 16:55:02 +0100

    First update

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import SellerForm from './components/Sellerform';
+import BuyerForm from './components/Buyerform';
+import LandingPage from './components/LandingPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Navbar />
+        <main>
+          <Routes>
+            <Route path="/seller" element={<SellerForm />} />
+            <Route path="/buyer" element={<BuyerForm />} />
+            <Route path="/LandingPage" element={<LandingPage />}/>
+          </Routes>
+        </main>
+
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
